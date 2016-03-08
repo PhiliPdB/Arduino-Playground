b6ce3ef52e942e4467c066d574b04e3924f9b557
PhiliPdB <phlpdbrn@gmail.com>
2016-03-08 10:44:22 +0100

    Correct setup lcd display

--- source/source.ino
+++ source/source.ino
@@ -4,9 +4,11 @@ LiquidCrystal lcd(7,8,9,10,11,12);
 
 void setup() {
   // put your setup code here, to run once:
-  lcd.begin(16,1);
-  lcd.setCursor(0,1);
+  lcd.begin(16,2);
+  lcd.setCursor(0,0);
   lcd.write("Hello World!");
+  lcd.setCursor(0,1);
+  lcd.write("Temp: ");
 }
 
 void loop() {
