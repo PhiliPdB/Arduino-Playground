cac9c9273a447630b8dfad60cdef0eae5531ec85
PhiliPdB <phlpdbrn@gmail.com>
2016-03-08 11:25:02 +0100

    Setup (not working) DHT11

--- source/source.ino
+++ source/source.ino
@@ -1,17 +1,54 @@
 #include <LiquidCrystal.h>
+#include <DHT.h>
 
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+// Initialize LCD display.
 LiquidCrystal lcd(7,8,9,10,11,12);
+// Initialize DHT sensor.
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   // put your setup code here, to run once:
   lcd.begin(16,2);
   lcd.setCursor(0,0);
-  lcd.write("Hello World!");
-  lcd.setCursor(0,1);
   lcd.write("Temp: ");
+  lcd.setCursor(0,1);
+  lcd.write("Humidity: ");
+
+  Serial.begin(9600);
+  Serial.println("WeatherStation:");
+
+  dht.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Wait a few seconds between measurements.
+  delay(2000);
 
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  int h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  int t = dht.readTemperature();
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.println("%");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.println(" *C");
+  Serial.print("Heat index: ");
+  Serial.print(hic);
+  Serial.println(" *C");
 }
