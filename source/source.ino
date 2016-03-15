6ed218dacc569e5b0421b8a5083f41cf20df2941
PhiliPdB <phlpdbrn@gmail.com>
2016-03-15 10:29:31 +0100

    Display temperature and humidity

--- source/source.ino
+++ source/source.ino
@@ -24,9 +24,13 @@ void setup() {
 }
 
 void loop() {
+  readDHT();
+
   // Wait a few seconds between measurements.
   delay(2000);
+}
 
+void readDHT() {
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   int h = dht.readHumidity();
@@ -45,4 +49,26 @@ void loop() {
   Serial.print("Temperature: ");
   Serial.print(t);
   Serial.println(" *C");
+
+  if (t < 10) {
+    lcd.setCursor(13,0);
+  } else {
+    lcd.setCursor(12,0);
+  }
+  lcd.print(t);
+  lcd.print((char) 223);
+  lcd.print("C");
+
+  if (h < 100) {
+    if (h < 10) {
+      lcd.setCursor(14,1);
+    } else {
+      lcd.setCursor(13,1);
+    }
+  } else {
+    lcd.setCursor(12,1);
+  }
+  lcd.print(h);
+  lcd.print("%");
 }
+
